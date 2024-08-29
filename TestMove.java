00a91963ee6b2cfa58df6602557112c9855c0d4d
Sandaruwan Weerakkody <154538944+SandaruwanWeerakkody@users.noreply.github.com>
2024-08-29 19:42:09 +0530

    Create TestMove.java

---
+++ TestMove.java
@@ -0,0 +1,8 @@
+public class TestMove {
+    public static void main(String[] args) {
+        Point p1 = new Point(1,2);
+        Movable m1 = new Movable(2,3);
+        m1.move();
+        m1.move();
+    }
+}
